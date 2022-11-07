8269fad2930c57c7bcc94e3c73a295ed48e393e6
Darian Stephens <965181fb@opayq.com>
2022-11-08 06:30:44 +1100

    I meant to commit these to the scripts folder
    I will reupload in the right place this time

--- prefabs/vampirebatcave_potential.lua
+++
@@ -1,8 +0,0 @@
-local function fn(Sim)
-	local inst = CreateEntity()
-	local trans = inst.entity:AddTransform()
-	inst:AddTag("cave_potential")
-    return inst
-end
-
-return Prefab( "common/objects/vampirebatcave_potential", fn ) 
