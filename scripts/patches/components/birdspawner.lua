ab548838f9341f1506342f21a4cc536b88d79343
Darian Stephens <965181fb@opayq.com>
2023-03-13 14:33:32 +1100

    IA Compatibility
    The birdspawner patch was crashing/causing a memory leak, because IA restructured the function that UpvalueHacker was using.

--- scripts/patches/components/birdspawner.lua
+++ scripts/patches/components/birdspawner.lua
@@ -1,4 +1,10 @@
 return function(self)
-	local BIRD_TYPES = UpvalueHacker.GetUpvalue(self.SpawnBird, "PickBird", "BIRD_TYPES")
+	local isIAEnabled = KnownModIndex:IsModEnabled("workshop-1467214795")
+	local BIRD_TYPES = nil
+	if not isIAEnabled then
+		BIRD_TYPES = UpvalueHacker.GetUpvalue(self.SpawnBird, "PickBird", "BIRD_TYPES")
+	else 
+		BIRD_TYPES = UpvalueHacker.GetUpvalue(self.SpawnBird, "birdvstile")
+	end
 	BIRD_TYPES[WORLD_TILES.INTERIOR] = {}   --Stop spawning birds in houses
 end
